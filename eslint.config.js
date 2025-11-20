8475be683c49214d1ed1af77a526507d28994381
xu.chen <xc13279112887@gmail.com>
2025-11-20 16:48:27 +0800

    fix(lint): 使用ts库中的no-unused-vars规则

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
+import {defineConfig, globalIgnores} from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 
@@ -16,11 +16,21 @@ const eslintConfig = defineConfig([
   {
     rules: {
       eqeqeq: 'off',
-      'no-unused-vars': ['error', { 'varsIgnorePattern': '^_' }],
       'prefer-const': ['error', { 'ignoreReadBeforeAssign': true }],
       indent: ['error', 2],
       quotes: ['error', 'single'],
-      'object-curly-spacing': ['error', 'always']
+      'object-curly-spacing': ['error', 'always'],
+      // 关闭 JS 原生的 no-unused-vars 规则 (无法理解Ts, 会误报 Type 定义)
+      'no-unused-vars': 'off',
+      // 开启 TS 专用规则 @typescript-eslint/no-unused-vars
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'argsIgnorePattern': '^_', // 忽略以 _ 开头的参数
+          'varsIgnorePattern': '^_', // 忽略以 _ 开头的变量
+          'caughtErrorsIgnorePattern': '^_', // 忽略以 _ 开头的 catch 错误
+        },
+      ],
     },
   },
 ]);
